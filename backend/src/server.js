e0bf09151441b065cb9f25ea4585530c03a2378c
Bolaji akintunde <bolajiakintunde216@gmail.com>
2026-05-03 16:30:34 +0100

    Add server listening log message

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,9 @@ const app = require('./app');
 const { testConnection } = require('./config/database');
 
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 const start = async () => {
   const dbOk = await testConnection();
